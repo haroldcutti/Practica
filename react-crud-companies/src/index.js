4f5b0493469364d6291dbc6b8dda03962b00cede
haroldcutti <harold.cutti@tecsup.edu.pe>
2023-10-28 17:59:57 -0500

    Se agrega el ejemplo

---
+++ react-crud-companies/src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import reportWebVitals from './reportWebVitals';
+
+//Componentes:
+import Navbar from './components/Navbar/Navbar';
+import CompanyList from './components/Company/CompanyList';
+import CompanyForm from './components/Company/CompanyForm';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+<BrowserRouter>
+    <Navbar/>
+        <div className='container my-4'>
+            <Switch>
+                <Route exact path='/' component={CompanyList} />
+                <Route path='/companyForm' component={CompanyForm} />
+                <Route path='/updateCompany/:id' component={CompanyForm} />
+            </Switch>
+        </div>
+</BrowserRouter>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
